266d9803f009a1141ab8cd6495ef9ee0af816da4
szymonator <23020097@woodhouse.ac.uk>
2026-02-21 18:20:38 +0000

    added gitignore, testing opencv capabilities

--- main.py
+++ main.py
@@ -2,23 +2,25 @@ import numpy as np
 import cv2 as cv
  
 cap = cv.VideoCapture(0)
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
  
-    # if frame is read correctly ret is True
+# Define the codec and create VideoWriter object
+fourcc = cv.VideoWriter_fourcc(*'MJPG')
+out = cv.VideoWriter('output.avi', fourcc, 30, (640,  480))
+ 
+while cap.isOpened():
+    ret, frame = cap.read()
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-
-    # Display the resulting frame
-    cv.imshow('Gesture Detector', frame)
+ 
+    # write the flipped frame
+    out.write(frame)
+ 
+    cv.imshow('frame', frame)
     if cv.waitKey(1) == ord('q'):
         break
  
-# When everything done, release the capture
+# Release everything if job is finished
 cap.release()
+out.release()
 cv.destroyAllWindows()
